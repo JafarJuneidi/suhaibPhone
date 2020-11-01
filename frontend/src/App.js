23b3f220338cd95b7bcd1d3c77bca30547fe9649
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-01 15:09:02 +0200

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
     return (
         // Empty element (fragment)
-        <>  
+        <>
             <Header />
             <main className='py-3'>
                 <Container>
-                    <h1>Welcome To Proshop</h1>
+                    <HomeScreen />
                 </Container>
             </main>
             <Footer />
         </>
     );
-}
+};
 
 export default App;
