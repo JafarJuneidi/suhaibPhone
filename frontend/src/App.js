b6e8512ffd87c55567e863f6094fe30b8fd1c3cb
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-01 15:59:59 +0200

    Implementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
     return (
         // Empty element (fragment)
-        <>
+        <BrowserRouter>
             <Header />
             <main className='py-3'>
                 <Container>
-                    <HomeScreen />
+                    {/* exact fixes the need for switch */}
+                    <Route path='/' component={HomeScreen} exact />
+                    <Route path='/product/:id' component={ProductScreen} />
                 </Container>
             </main>
             <Footer />
-        </>
+        </BrowserRouter>
     );
 };
 
