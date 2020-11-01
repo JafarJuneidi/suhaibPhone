cc2ea108c9d70345e34ae98f41db183f95eaa8d3
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-01 11:07:51 +0200

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        // Empty element (fragment)
+        <>
+            <h1>Welcome To Proshop</h1>
+        </>
+    );
+}
+
+export default App;
