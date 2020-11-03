050314242c3ed3f84e2d30c7ba47cbb13cf4d082
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-03 14:49:58 +0200

    Fetching Products From The Database

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 // need to add .js when importing js files
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -15,14 +15,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(
     PORT,
