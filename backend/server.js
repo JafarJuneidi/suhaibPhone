7aff332d2c7ec86bfed4efbb8e77c77a545c03ab
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-03 09:12:14 +0200

    Connecting To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 // need to add .js when importing js files
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API is running...');
