2eabe7a667c52848ee8a263642f6c9db59917e57
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-03 09:29:30 +0200

    Connecting To The Database (modified)

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,6 +13,21 @@ const connectDB = async () => {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
+
+    // My code without async
+    // mongoose
+    //     .connect(process.env.MONGO_URI, {
+    //         useUnifiedTopology: true,
+    //         useNewUrlParser: true,
+    //         useCreateIndex: true,
+    //     })
+    //     .then((res) => {
+    //         console.log(`mongoDB connected: ${res.connection.host}`);
+    //     })
+    //     .catch((error) => {
+    //         console.error(`Error: ${error.message}`);
+    //         process.exit(1);
+    //     });
 };
 
 export default connectDB;
