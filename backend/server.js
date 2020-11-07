b945d9900860c391b8f867a073eb42dd7b58ac08
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-07 16:50:33 +0200

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,22 @@ import colors from 'colors';
 // need to add .js when importing js files
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
-const app = express();
-
 connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Middleware
 app.use(notFound);
