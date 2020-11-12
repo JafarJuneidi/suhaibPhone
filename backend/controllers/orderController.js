496e319eb4b95030ad7915cf0fde496056152f99
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-12 17:39:52 +0200

    Get Order by ID Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,4 +38,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
     }
 });
 
-export { addOrderItems };
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    // populate get user name and email that is associated with this order
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    );
+
+    if (order) {
+        res.json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+export { addOrderItems, getOrderById };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+// make sure this is at the bottom, so that when it looks at what's afte / it's an ID
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
