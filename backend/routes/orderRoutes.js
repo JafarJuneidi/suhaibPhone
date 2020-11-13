6fda139f735271ca8114756d1f27c0a6c1a66015
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-13 12:11:42 +0200

    Show Orders On Profile

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,12 @@ import {
     addOrderItems,
     getOrderById,
     updateOrderToPaid,
+    getMyOrders,
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/myorders').get(protect, getMyOrders);
 // make sure this is at the bottom, so that when it looks at what's afte / it's an ID
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
