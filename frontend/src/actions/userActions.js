f79b9d1ab1f3147ec4518267e376fda2222b1819
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-13 12:25:47 +0200

    User Details & Orders Reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -15,6 +16,7 @@ import {
     USER_UPDATE_PROFILE_RESET,
     USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -55,6 +57,14 @@ export const logout = () => async (dispatch) => {
     localStorage.removeItem('userInfo');
     dispatch({
         type: USER_LOGOUT,
+    });
+
+    dispatch({
+        type: USER_DETAILS_RESET,
+    });
+
+    dispatch({
+        type: ORDER_LIST_MY_RESET,
     });
 };
 
