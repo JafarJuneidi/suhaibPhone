6316fdae575c6f169d8454c472bf03b82f736b92
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-13 08:44:55 +0200

    Update To Paid Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 // make sure this is at the bottom, so that when it looks at what's afte / it's an ID
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
