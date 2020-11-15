55b6f70c5eac90f5055f16391649be915921a4bc
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-15 15:06:54 +0200

    User Edit Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const App = () => {
     return (
@@ -33,6 +34,10 @@ const App = () => {
                     {/* ? makes the id optional so we can access cart from icon */}
                     <Route path='/cart/:id?' component={CartScreen} />
                     <Route path='/admin/userlist' component={UserListScreen} />
+                    <Route
+                        path='/admin/user/:id/edit'
+                        component={UserEditScreen}
+                    />
                     {/* exact fixes the need for switch */}
                     <Route path='/' component={HomeScreen} exact />
                 </Container>
