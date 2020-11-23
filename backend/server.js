920e232ea6ca3c0cbef06e5a11a724546e5ebd94
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2020-11-23 12:51:03 +0200

    Morgan & Create Review Endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 // need to add .js when importing js files
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -14,6 +15,10 @@ dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
