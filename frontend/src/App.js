d37cb28a224e8e05f5cdca00b516f5f4c9a9ea26
Jafar <aljuneidi.jafar@gmail.com>
2022-11-16 20:59:47 +0200

    preparing to host on vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,61 +19,48 @@ import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
 
 const App = () => {
-    return (
-        // Empty element (fragment)
-        <BrowserRouter>
-            <Header />
-            <main className='py-3'>
-                <Container>
-                    <Route path='/login' component={LoginScreen} />
-                    <Route path='/shipping' component={ShippingScreen} />
-                    <Route path='/placeorder' component={PlaceOrderScreen} />
-                    <Route path='/order/:id' component={OrderScreen} />
-                    <Route path='/register' component={RegisterScreen} />
-                    <Route path='/profile' component={ProfileScreen} />
-                    <Route path='/product/:id' component={ProductScreen} />
-                    {/* ? makes the id optional so we can access cart from icon */}
-                    <Route path='/cart/:id?' component={CartScreen} />
-                    <Route path='/admin/userlist' component={UserListScreen} />
-                    <Route
-                        path='/admin/user/:id/edit'
-                        component={UserEditScreen}
-                    />
-                    <Route
-                        path='/admin/productlist'
-                        component={ProductListScreen}
-                        exact
-                    />
-                    <Route
-                        path='/admin/productlist/:pageNumber'
-                        component={ProductListScreen}
-                        exact
-                    />
-                    <Route
-                        path='/admin/product/:id/edit'
-                        component={ProductEditScreen}
-                    />
-                    <Route
-                        path='/admin/orderlist'
-                        component={OrderListScreen}
-                    />
-                    <Route
-                        path='/search/:keyword'
-                        component={HomeScreen}
-                        exact
-                    />
-                    <Route
-                        path='/search/:keyword/page/:pageNumber'
-                        component={HomeScreen}
-                    />
-                    <Route path='/page/:pageNumber' component={HomeScreen} />
-                    {/* exact fixes the need for switch */}
-                    <Route path='/' component={HomeScreen} exact />
-                </Container>
-            </main>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    // Empty element (fragment)
+    <BrowserRouter basename='/'>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          {/* ? makes the id optional so we can access cart from icon */}
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route
+            path='/admin/productlist'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+          />
+          <Route path='/page/:pageNumber' component={HomeScreen} />
+          {/* exact fixes the need for switch */}
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
